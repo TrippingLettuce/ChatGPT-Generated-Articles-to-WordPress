a308bbb2ca46370c43c66b4a3f0ccdfe051eb261
Caleb Klinger <82426784+TrippingLettuce@users.noreply.github.com>
2023-01-18 18:14:37 -0700

    Update wordpAPI.php

--- wordpAPI.php
+++ wordpAPI.php
@@ -8,7 +8,7 @@ $options  = array (
  
 $context  = stream_context_create( $options );
 $response = file_get_contents(
-    'https://public-api.wordpress.com/rest/v1/sites/testing9035.wordpress.com/',
+    'https://public-api.wordpress.com/rest/v1/sites/MySite.wordpress.com/',
     false,
     $context
 );
