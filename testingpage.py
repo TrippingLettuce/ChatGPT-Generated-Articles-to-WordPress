c6e098d3a2d256723b9df29cb877591f7348824b
Caleb Klinger <82426784+TrippingLettuce@users.noreply.github.com>
2023-01-18 18:13:47 -0700

    Update testingpage.py

--- testingpage.py
+++ testingpage.py
@@ -2,15 +2,15 @@ import requests
 import base64
 import json
 
-username = "klingercaleb11"
-password = "sfLj5j9ry67sYLd"
+username = ""
+password = ""
 
 creds = username + ':' + password
 cred_token = base64.b64encode(creds.encode())
 
 header = {'Authorization': 'Basic ' + cred_token.decode('utf-8')}
 
-url = "https://testing9035.wordpress.com/wp/v2"
+url = "https://MyWEBSITE.wordpress.com/wp/v2"
 
 postID = "2"
 post = {
