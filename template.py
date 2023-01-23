53ebd2e582ae72c14606bfa6743a47af7ad674fb
Caleb Klinger <82426784+TrippingLettuce@users.noreply.github.com>
2023-01-22 23:10:04 -0700

    Update template.py

--- template.py
+++ template.py
@@ -1,29 +1,38 @@
 import requests
 import AIAPI
+import imgAPI
 
-place = "Tokyo"
-prompt = f"Generate a travel article about {place}"
-#Generate from block to get title
-title_prompt = f"Generate a travel article title for {place}"
+
+place = "Place"
+prompt = f"Generate a travel guild about {place}"
+title_prompt = f"Generate a travel guild title for {place}"
+img_prompt = f"{place} trip"
+
 
 article_block_content = AIAPI.article_block_generate(prompt)
 article_title = AIAPI.article_title_generate(title_prompt)
+img = imgAPI.get_img(img_prompt)
 
-ROOT = 'https://lettuce.website'
+
+ROOT = 'https://website.com'
 user = 'Username'
-password = 'Password'
-headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+password = 'Password' 
+#Use Application Password for Password Section User>Profile>Add New Password Name
 
-#Generate a Header
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 data = {
     'title' : f'{article_title}',
-    'content' : f'{article_block_content}',
-    'status' : 'draft'
-}
-
-#Draft if wanted
+    'content' : f'<img src={img} width="900" height="400" />{article_block_content}',
+    'status' : 'draft' #or publish 
+    }
 #template if theme
 
+#/posts for post, /page for new page, /posts/id for update and del post
 r = requests.post(url= ROOT + '/wp-json/wp/v2/posts', data=data, headers=headers, auth=(user, password))
 print(r)
+#404 means https URL cant be found
+#400-401 https URL cant connect (Username/Password) Error
+#201 It worked :)
+#200 Connected but nothing sent
+
 
